193e0a047ba95ae1c37226866185e11a56f758ee
An Byungwook <abw2619@icloud.com>
2023-02-06 13:10:36 +0900

    Initial Commit

---
+++ Eco-friendlyStep/Eco_friendlyStepApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eco_friendlyStepApp.swift
+//  Eco-friendlyStep
+//
+//  Created by 안병욱의 mac on 2023/02/06.
+//
+
+import SwiftUI
+
+@main
+struct Eco_friendlyStepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
